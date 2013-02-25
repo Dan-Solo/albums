223971eb39b0f82056ea70c56e498a38d3821540
Dan <dan@cjsdigital.org>
2013-02-25 12:37:00 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Albums::Application.config.secret_token = 'ed2553b2d0e601d0dda65acdedeeab0c9466ba6e4bb940304a32fdeb78902dd46fdaeacff048dac87828cc10cf830bf0e8a435dcf8f1308b873e67f049923ad1'
